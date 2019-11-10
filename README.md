6e0bb458e6a1bd3431f8539f94a5f74893dbacd9
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-11-10 18:26:13 +0100

    A fresh new start

---
+++ README.md
@@ -0,0 +1,6 @@
+# COUNTDOWWN TO 2020
+
+> just to make sure we don't miss anything!
+
+Enjoy!
+Roberto
---
+++ app.js
@@ -0,0 +1,31 @@
+function countdown() {
+    let today = new Date();
+    let eventDate = new Date('january 1,2020 00:00:00');
+
+    let currentTime = today.getTime();
+    let eventTime = eventDate.getTime();
+
+    let leftTime = eventTime - currentTime;
+
+    let sec = Math.floor(leftTime/1000);
+    let min = Math.floor(sec/60);
+    let hrs = Math.floor(min/60);
+    let days = Math.floor(hrs/24);
+
+    hrs %= 24;
+    min %= 60;
+    sec %= 60;
+
+    hrs = (hrs < 10) ? "0" + hrs : hrs;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
