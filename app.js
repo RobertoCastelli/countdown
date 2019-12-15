a2bef41e1531d65075f40c724134712e6e080931
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-12-15 22:25:14 +0100

    just for Na

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function countdown() {
+    
     let today = new Date();
-    let eventDate = new Date('january 1,2020 00:00:00');
+    let eventDate = new Date('december 18,2019 23:59:59');
 
     let currentTime = today.getTime();
     let eventTime = eventDate.getTime();
@@ -19,6 +20,15 @@ function countdown() {
     hrs = (hrs < 10) ? "0" + hrs : hrs;
     min = (min < 10) ? "0" + min : min;
     sec = (sec < 10) ? "0" + sec : sec;
+
+    if (sec == 0 && min ==0 && hrs == 0 && days == 0 ) {
+        alert('https://to-naajeni.netlify.com/');
+        sec = 0
+        min = 0
+        hrs = 0
+        days = 0
+    } 
+        
 
     document.getElementById('days').innerHTML = days;
     document.getElementById('hours').innerHTML = hrs;
